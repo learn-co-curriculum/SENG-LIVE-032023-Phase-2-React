cab9e4d1a7306dbc66f2b318dd7a416d0f77c523
Princeton J. Rose <princetonjrose@gmail.com>
2023-04-13 17:09:19 -0400

    got most of the way...

---
+++ Lab Reviews/react-hooks-state-events-mini-project/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import CategoryFilter from "./CategoryFilter";
+import NewTaskForm from "./NewTaskForm";
+import TaskList from "./TaskList";
+
+import { CATEGORIES, TASKS } from "../data";
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
+function App() {
+
+  const [ tasks, setTasks ] = useState( TASKS )
+  const [ categories, setCategories ] = useState( CATEGORIES )
+
+  const [ selectedCategory, setSelectedCategory ] = useState( 'All' )
+
+  const changeSelectedCategory = newCategory => setSelectedCategory( newCategory )
+
+  const deleteTask = ( deleteIndex ) => {
+    const removeTaskFromList = tasks.filter( ( task, index ) => index !== deleteIndex )
+    
+    setTasks( removeTaskFromList )
+  }
+
+  const filterTasks = tasks.filter( task => task.category === selectedCategory || selectedCategory === 'All' )
+
+  return (
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter
+        categories = { categories }
+        changeSelectedCategory = { changeSelectedCategory }
+      />
+      <NewTaskForm />
+      <TaskList 
+        tasks = { filterTasks } 
+        deleteTask = { deleteTask }
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab Reviews/react-hooks-state-events-mini-project/src/components/CategoryFilter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function CategoryFilter( props ) {
+
+  const renderCategories = props.categories.map( category => 
+    <button 
+      key={category} 
+      onClick={ ()=> props.changeSelectedCategory( category ) }
+    >{ category }</button>
+  )
+  return (
+    <div className="categories">
+      <h5>Category filters</h5>
+      {/* render <button> elements for each category here */}
+      { renderCategories }
+    </div>
+  );
+}
+
+export default CategoryFilter;
